15dd93c0f45ed3b1a8d179103af370296368ead6
Divyanshu Gupta <guptadivyanshu29@gmail.com>
2021-11-01 09:39:55 +0530

    first commit

---
+++ app/src/main/java/com/example/covid19tracker/AffectedCountries.java
@@ -0,0 +1,128 @@
+package com.example.covid19tracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.eazegraph.lib.models.PieModel;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AffectedCountries extends AppCompatActivity {
+    EditText edtSearch;
+    ListView listView;
+    SimpleArcLoader simpleArcLoader;
+
+    public List<CountryModel> countryModelList = new ArrayList<CountryModel>();
+    CountryModel countryModel;
+    MyCustomAdapter myCustomAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affected_countries);
+        edtSearch =findViewById(R.id.editSearch);
+        listView = findViewById(R.id.ListView);
+        simpleArcLoader = findViewById(R.id.loader);
+
+        getSupportActionBar().setTitle("Affected Countries");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        fetchData();
+
+        edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                myCustomAdapter.getFilter().filter(charSequence);
+                myCustomAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void fetchData() {
+        String url = "https://disease.sh/v3/covid-19/countries";
+        simpleArcLoader.start();
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = new JSONArray(response);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String countryName = jsonObject.getString("country");
+                        String cases = jsonObject.getString("cases");
+                        String todayCases = jsonObject.getString("todayCases");
+                        String deaths = jsonObject.getString("deaths");
+                        String todayDeaths = jsonObject.getString("todayDeaths");
+                        String recovered = jsonObject.getString("recovered");
+                        String active = jsonObject.getString("active");
+                        String critical = jsonObject.getString("critical");
+                        JSONObject object = jsonObject.getJSONObject("countryInfo");
+                        String flagUrl = object.getString("flag");
+                        countryModel = new CountryModel(flagUrl,countryName,cases,todayCases,todayDeaths,deaths,recovered,active,critical);
+                        countryModelList.add(countryModel);
+                    }
+                    myCustomAdapter = new MyCustomAdapter(AffectedCountries.this,countryModelList);
+                    listView.setAdapter(myCustomAdapter);
+                    simpleArcLoader.stop();
+                    simpleArcLoader.setVisibility(View.GONE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    simpleArcLoader.stop();
+                    simpleArcLoader.setVisibility(View.GONE);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(AffectedCountries.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+    }
+}
